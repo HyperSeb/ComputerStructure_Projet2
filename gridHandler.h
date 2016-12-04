5865dc4aa8f401ffc6030c02c0986ea53c966751
NatGr <nathan.2@live.be>
2016-12-04 10:41:02 +0100

    Update gridHandler.h

--- gridHandler.h
+++ gridHandler.h
@@ -1,6 +1,10 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+/* turns the index of an element in a matrix of given width
+into the index of the same element in a table representing the matrix
+*/
+int ind(int i,int j, int width);
 
 // generates a random grid of height M and width N
 void randomGrid(unsigned int M, unsigned int N);
