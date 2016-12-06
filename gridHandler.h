1160061974182da9eed5623460c23727b066e913
NatGr <nathan.2@live.be>
2016-12-06 18:35:30 +0100

    Update gridHandler.h

--- gridHandler.h
+++ gridHandler.h
@@ -6,10 +6,13 @@ into the index of the same element in a table representing the matrix
 */
 int ind(int i,int j, int width);
 
-// generates a random grid of height M and width N 
-// (we consider here the outer "walls" to be part of the table)
-// so N and M must have been incremented in the main function
+/* generates a random grid of height M and width N 
+(we consider here the outer "walls" to be part of the table)
+so N and M must have been incremented in the main function
+*/
 void randomGrid(unsigned int M, unsigned int N);
 
-// generates a grid of height M and width N from a file named "name"
-void GridFromFile(unsigned int M, unsigned int N, char* name);
+/* generates a grid of height M and width N from a file named "name"
+return -1 if an error occured
+*/
+int GridFromFile(unsigned int M, unsigned int N, char* name);
