32f80f4a2fbad3615b62ee34a732fceababe31be
Sébastien Blondiau <sebastien.blondiau@me.com>
2016-12-10 18:11:27 +0100

    Update process.h

--- process.h
+++ process.h
@@ -27,14 +27,14 @@ int* genomeAtIndex(Genomes genomes, int index);
 /* a process which goal is to listen to the user commands, to print the journey 
 of the best creature so far and to close the progam
 */
-void listenerProcess(Grid grid, int numberOfSlaves, int genomeLength);
+void listenerProcess(Grid grid, Genomes genomes, int numberOfSlaves);
 
 /* a process which goal is to compute the score of a creature which index is 
 given by the master process through a message queue
 */
-void workerProcess(Grid grid, int genomeLength);
+void workerProcess(Grid grid, Genomes genomes);
 
 /* a process which goal is to handle the generation/classification/mutation of 
 the creatures
 */
-void masterProcess(int numberOfSlaves, int numberOfCreature, int deletionRate, int mutationRate, int genomeLength);
+void masterProcess(int numberOfSlaves, int deletionRate, int mutationRate, Genomes genomes);
