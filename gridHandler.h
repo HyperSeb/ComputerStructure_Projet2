97e124bbcb71a6585b67a4c5790fd19bd3b04dac
NatGr <nathan.2@live.be>
2016-12-16 09:55:05 +0100

    Update gridHandler.h

--- gridHandler.h
+++ gridHandler.h
@@ -8,16 +8,17 @@
 // the value representing an obstacle in the grid
 #define obstacle true
 
+// the x,y position of a tile
 struct Position {
     int x;
     int y;
 };
 typedef struct Position Position;
 
-/* check if two positions are the same 
-*/
+// checks if two positions are the same 
 bool equalPos(Position p1, Position p2);
 
+// a structure representing the grid
 struct Grid {
     bool* storage;
     int width;
@@ -27,27 +28,22 @@ struct Grid {
 };
 typedef struct Grid Grid;
 
-/* return the element of the grid at the given position
-*/
+// returns the element of the grid at the given position
 bool getInGrid(Grid grid, Position position);
 
-/* return the element of the grid at the given position
-*/
+// sets the element of the grid at the given position
 void setInGrid(Grid grid, Position position, bool value);
 
-/* fill the grid randomly, and set start and finish at random
-*/
+// fills the grid randomly, and set start and finish at random
 void fillGridRandomly(Grid* grid);
 
 /* fill the grid of the given size with content from a file named "name"
-checks if there is a border of tiles and if there is no tile over the
-begin/end flag
-return -1 if an error occured
+checks if there is no tile over the begin/end flag,
+returns -1 if an error occured
 */
 int fillGridWithFile(Grid* grid, char* name);
 
-/* display the grid on stdout
-*/
+// display the grid on stdout
 void displayGrid(Grid grid, Position creaturePosition, int* genome, int genomeLength, int geneIndex);
 
 
