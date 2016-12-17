e0e0dca8881c9555442d049fa75516796d853143
Sébastien Blondiau <sebastien.blondiau@me.com>
2016-12-17 21:15:16 +0100

    move functions in creature.c

--- process.c
+++ process.c
@@ -64,145 +64,6 @@ static void copyGenome(int* from, int* to, int genomeLength) {
 	}
 }
 
-// tests whether the first integer is greater than the second
-static bool moreThanHalf(int half, int full) {
-    if (full == 0) {
-        return true;
-    } else if (full > 0){
-        return 2 * half >= full;
-    } else {
-        return 2 * half <= full;
-    }
-}
-
-// computes the result of a move
-// assuming deltaX = -1, 0 or 1, else undefined behaviour may appear
-static Position computeResultOfMove(Grid grid, Position from, int deltaX, int deltaY) {
-    int verticalDirection = deltaY < 0 ? -1 : 1;
-    
-    if (deltaX == 0) {
-        // vertical move, go forward until end or obstacle
-        for (int dy = 0; dy * verticalDirection < deltaY * verticalDirection; dy += verticalDirection) {
-            Position further = {from.x, from.y + dy + verticalDirection};
-            
-            if (getInGrid(grid, further) == obstacle) {
-                Position current = {from.x, from.y + dy};
-                return current;
-            }
-        }
-        
-        Position current = {from.x, from.y + deltaY};
-        return current;
-        
-    } else if (deltaY == 0) {
-        // horizontal move, the destination is either an obstacle or not
-        Position further = {from.x + deltaX, from.y};
-        if (getInGrid(grid, further) == obstacle) {
-            return from;
-        } else {
-            return further;
-        }
-    } else {
-        // diagonal move, go vertically forward,
-        // if there is an obstacle in the column or in the column in the direction of deltaX, a special comportement is performed
-        for (int dy = 0; dy * verticalDirection < deltaY * verticalDirection; dy += verticalDirection) {
-            Position current = {from.x + (moreThanHalf(dy, deltaY) ? deltaX : 0), from.y + dy};
-            
-            Position further = {from.x, from.y + dy + verticalDirection},
-            nextToFurther = {from.x + deltaX, from.y + dy + verticalDirection};
-            
-            if (getInGrid(grid, further) == obstacle && getInGrid(grid, nextToFurther) == obstacle) {
-                // both are obstacle, don't go further
-                return current;
-                
-            } else if (getInGrid(grid, further) == obstacle) {
-                // the one in the column we start the move is an obstacle,
-                // depending on how far we are in the move, we land on the obstacle, or continue
-                if (moreThanHalf(dy + verticalDirection, deltaY)) {
-                    continue;
-                } else {
-                    return current;
-                }
-                
-            } else if (getInGrid(grid, nextToFurther) == obstacle) {
-                // the one in the column we are going is an obstacle,
-                // depending on how far we are in the move, we land on it, or fall vertically
-                if (moreThanHalf(dy + verticalDirection, deltaY)) {
-                    if (moreThanHalf(dy, deltaY)) {
-                        return current;
-                    } else {
-                        return computeResultOfMove(grid, current, 0, deltaY - dy); // the rest of the move is vertical
-                    }
-                } else {
-                    return computeResultOfMove(grid, current, 0, deltaY - dy); // the rest of the move is vertical
-                }
-                
-            } else {
-                // no obstacle, we continue
-                continue;
-            }
-        }
-        
-        // no problematic obstacle were met, the move is completely done
-        Position current = {from.x + deltaX, from.y + deltaY};
-        return current;
-    }
-}
-
-// returns the final position of the creature
-static Position performCreature(Grid grid, int* genome, int genomeLength, bool displayingSteps) {
-    Position currentPosition = grid.start;
-
-    for (int geneIndex = -1; geneIndex < genomeLength; geneIndex++) {
-        int direction = geneIndex == -1 ? -1 : genome[geneIndex];
-        int deltaX, deltaY;
-        
-        switch (direction) {
-                
-#define MOVE(dX, dY, intValue, character) \
-            case intValue:\
-                deltaX = dX;\
-                deltaY = dY;\
-                break;
-#include "move.txt"
-                
-            default:
-                deltaX = 0;
-                deltaY = 0;
-                break;
-        }
-        Position underPosition;
-        do {
-            Position nextPosition = computeResultOfMove(grid, currentPosition, deltaX, deltaY);
-            
-            deltaX = nextPosition.x - currentPosition.x; // the fall continue to be vertical if the partial move was
-            deltaY--; // gravity
-            
-            currentPosition = nextPosition;
-            
-            if (displayingSteps) {
-                printf("\n\n\n\n\n\n");
-                displayGrid(grid, currentPosition, genome, genomeLength, geneIndex);
-                sleep(1);
-            }
-            
-            underPosition.x = currentPosition.x;
-            underPosition.y = currentPosition.y - 1;
-
-        } while (getInGrid(grid, underPosition) != obstacle);
-    }
-    
-    if (displayingSteps) {
-        printf("The journey is ended!\n");
-    }
-    return currentPosition;
-}
-
-// show the best creature's movements on the terminal
-static void showBest(Grid grid, int* genome, int genomeLength){
-	performCreature(grid, genome, genomeLength, true);
-}
-
 void listenerProcess(Grid grid, Genomes genomes, int numberOfSlaves, int qId, int semId, BestAndStop * sharedStruct){
 	bool stopAll = false;
 	printf("type: G to request a new generation\n");
@@ -269,16 +130,6 @@ void listenerProcess(Grid grid, Genomes genomes, int numberOfSlaves, int qId, in
 	msgctl(qId, IPC_RMID, 0);
 	return;
 }
-
-// computes the score of the creature
-static double computeScore(Grid grid, int* genome, int genomeLength) {
-    Position finalPosition = performCreature(grid, genome, genomeLength, false);
-    
-    int dX = finalPosition.x - grid.finish.x;
-    int dY = finalPosition.y - grid.finish.y;
-    
-    return sqrt(dX * dX + dY * dY);
-}
 	
 void workerProcess(Grid grid, Genomes genomes, double* scores, int qId, int semId, BestAndStop * sharedStruct){
 	int offset;
@@ -306,7 +157,7 @@ static void modifyCreature(int mutationRate, int* genome, int genomeLength){
 		if((rand() % 100) < mutationRate){ // if the move mutates
 			int newGene;
 			do {
-				newGene = rand()%8;
+				newGene = randomGene();
 			} while(genome[j] == newGene);
 			genome[j] = newGene;
 		}
@@ -316,7 +167,7 @@ static void modifyCreature(int mutationRate, int* genome, int genomeLength){
 // creates a new genome
 static void createCreature(int* genome, int genomeLength){
 	for(int j = 0; j < genomeLength; ++j){
-		genome[j] = rand()%8;
+		genome[j] = randomGene();
 	}
 }
 
